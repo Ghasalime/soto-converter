424cd552368f974184f78f7754682a2f093e3b2a
Ghasali <ghasali@ubuntupc.tail4de293.ts.net>
2026-03-30 16:32:52 +0700

    feat: add wordpress plugin download page and zip

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png', 'soto-webp-converter.zip'],
       workbox: {
         maximumFileSizeToCacheInBytes: 50 * 1024 * 1024 // 50 MB
       },
